e4f7b5f1f9c31cd056a0829138c511d162c97bd0
ADudys <ania.dudys99@gmail.com>
2023-10-29 12:09:04 +0100

    final fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,49 +2,43 @@ import { Statistics } from './Statistics/Statistics.jsx';
 import { FeedbackOptions } from './FeedbackOptions/FeedbackOptions.jsx';
 import { Notification } from './Notification/Notification.jsx';
 import { Section } from './Section/Section.jsx';
-import { useState } from 'react';
+import React, { useState } from 'react';
 
 export default function App() {
-  const [good, setGood] = useState(0);
-  const [neutral, setNutral] = useState(0);
-  const [bad, setBad] = useState(0);
+  const [state, setState] = useState({
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  });
 
-  const total = good + neutral + bad;
-  const positiveFeedback = Math.round((good / total) * 100);
+  const total = () => {
+    return Object.values(state).reduce((ac, item) => (ac += item));
+  };
+  const positiveFeedback = () => {
+    const res = Math.round((state.good / total()) * 100);
+    return !Number.isNaN(res) ? res : 0;
+  };
 
-  function onLeaveFeedback(e) {
-    switch (e.target.name) {
-      case 'good':
-        setGood(good + 1);
-        break;
-      case 'neutral':
-        setNutral(neutral + 1);
-        break;
-      case 'bad':
-        setBad(bad + 1);
-        break;
-
-      default:
-        break;
-    }
+  function onLeaveFeedback(option) {
+    setState((prev) => ({ ...prev, [option]: prev[option] + 1 }));
   }
+
+  const keys = Object.keys(state);
+  const { good, neutral, bad } = state;
 
   return (
     <>
       <Section title="Please leave feedback">
-        <FeedbackOptions
-          options={{ good, neutral, bad }}
-          onLeaveFeedback={onLeaveFeedback}
-        />
-        {total === 0 ? (
+        <FeedbackOptions options={keys} onLeaveFeedback={onLeaveFeedback} />
+        {total() === 0 ? (
           <Notification message="No feedback given"></Notification>
         ) : (
           <Statistics
             good={good}
             neutral={neutral}
             bad={bad}
-            total={total}
-            positivePercentage={positiveFeedback}
+            total={total()}
+            positivePercentage={positiveFeedback()}
           />
         )}
       </Section>
--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -2,22 +2,22 @@ import PropTypes from 'prop-types';
 import s from '../FeedbackOptions/FeedbackOptions.module.css';
 
 export const FeedbackOptions = ({ options, onLeaveFeedback }) => (
-  <div className={s.feedback__container}> 
-    {Object.keys(options).map(option => (
-    <button
-      key={option}
-      type="button"
-      name={option}
-      onClick={onLeaveFeedback}
-      className={s.feedback__btn}
-    >
+  <div className={s.feedback__container}>
+    {options.map(option => (
+      <button
+        key={option}
+        type="button"
+        name={option}
+        onClick={() => onLeaveFeedback(option)}
+        className={s.feedback__btn}
+      >
         {option}
-    </button>
-  ))}
+      </button>
+    ))}
   </div>
 );
 
 FeedbackOptions.propTypes = {
-    options: PropTypes.object.isRequired,
-    onLeaveFeedback: PropTypes.func.isRequired,
-}
+  options: PropTypes.object.isRequired,
+  onLeaveFeedback: PropTypes.func.isRequired,
+};
